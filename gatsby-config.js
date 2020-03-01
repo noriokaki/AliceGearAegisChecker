8f65bd215c05bc6308f348f8e11c4228e5e41c13
noriokaki <12603061+noriokaki@users.noreply.github.com>
2020-03-01 16:24:26 +0900

    gatsby他セットアップ

---
+++ gatsby-config.js
@@ -0,0 +1,42 @@
+/* eslint-disable */
+module.exports = {
+   siteMetadata: {
+      title: `アリス・ギア・アイギス アクトレス所属率チェッカー`,
+      description: `アリス・ギア・アイギスのアクトレスの所属率チェッカーです。`,
+      author: `@noriokaki`,
+   },
+   plugins: [
+      `gatsby-plugin-react-helmet`,
+      {
+         resolve: `gatsby-source-filesystem`,
+         options: {
+            name: `images`,
+            path: `${__dirname}/src/images`,
+         },
+      },
+      `gatsby-transformer-sharp`,
+      `gatsby-plugin-sharp`,
+      {
+         resolve: `gatsby-plugin-manifest`,
+         options: {
+            name: `gatsby-starter-default`,
+            short_name: `starter`,
+            start_url: `/`,
+            background_color: `#663399`,
+            theme_color: `#663399`,
+            display: `minimal-ui`,
+            icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+         },
+      },
+      {
+         resolve: "gatsby-plugin-graphql-codegen",
+         options: {
+            fileName: `types/graphql-types.d.ts`,
+         },
+      },
+      `gatsby-plugin-typescript`,
+      // this (optional) plugin enables Progressive Web App + Offline functionality
+      // To learn more, visit: https://gatsby.dev/offline
+      `gatsby-plugin-offline`,
+   ],
+};
