472e688a9306af04b8e3baa191c20e35b9a53448
noriokaki <12603061+noriokaki@users.noreply.github.com>
2021-10-21 12:37:45 +0900

    gatsbyのコンフィグから移動

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export default {
+   title: `アリス・ギア・アイギス スカウト率チェッカー`,
+   description: `アリス・ギア・アイギスのアクトレスのスカウト率チェッカーです。`,
+   author: `@noriokaki`,
+   url: "https://alice-gear-aegis-checker.netlify.com/",
+};
