5b79b2ea14cc160f2e47a9ac87378c36b3cee007
noriokaki <12603061+noriokaki@users.noreply.github.com>
2021-10-25 20:09:07 +0900

    getStaticProps内でNodeのモジュールを使えるようにする設定

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+module.exports = {
+   webpack5: true,
+   webpack: (config) => {
+      config.resolve.fallback = {
+         fs: false,
+         path: false,
+         process: false,
+      };
+
+      return config;
+   },
+};
